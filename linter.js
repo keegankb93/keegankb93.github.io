918d6b1ccebdd313dbb1b0c5f1490ea804bcc019
keegankb93 <80284046+keegankb93@users.noreply.github.com>
2021-04-01 13:03:34 -0500

    Add files via upload

--- linter.js
+++ linter.js
@@ -39,8 +39,17 @@ const fullEdit = string => {
 
 const textInput = document.querySelector('#text-input');
 const textOutput = document.querySelector('#text-output');
-textInput.addEventListener("keyup", () => {
+textInput.addEventListener('keyup', () => {
   textOutput.innerHTML = betterSentences(textInput.value);
 });
+
+textInput.addEventListener('focus', () => {
+  textInput.style.color = "black";
+  if(textInput.value === "Try using verbs like utilize and optimize or even inserting the dreaded oxford comma.") {
+    textInput.value= "";
+  }
+});
+
+
 //let sentence = "Hello! I **just** wanted Leverage and Circle back and let you know that I will utilize, optimize, and leverage my Rocket Referrals account. Hello, I **just**wanted leverage and Circle back and let you know that I will utilize, optimize, and leverage my Rocket Referrals account."
 //console.log(betterSentences(sentence));

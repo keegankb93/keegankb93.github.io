e8629071c5a7a71efbe8001620c14560214d4f36
Keegan <keegankb@gmail.com>
2021-04-06 15:43:19 -0500

    closure

--- linter.js
+++ linter.js
@@ -19,6 +19,7 @@ const initiate = () => {
   textInput().addEventListener('focusin', () => {
     textInput().style.color = "black";
     counter.style.visibility = "visible";
+  }
 
   textInput().addEventListener('input', () => {
     count(textInput().value);

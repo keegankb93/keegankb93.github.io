af1e5ef09b03771dc039daa9d42cff9bfc608dcf
Keegan <keegankb@gmail.com>
2021-04-10 12:58:22 -0500

    created editor object

--- linter.js
+++ linter.js
@@ -18,7 +18,6 @@ const settings = {
   },
   //changes the values of the setting based on the 'change' event on the setting toggle.
   changeSettings: function(event) {
-
     Object.keys(this).forEach(setting => {
       if (event.target.getAttribute("data-setting") === setting){
         if (event.target.checked) {
@@ -32,74 +31,74 @@ const settings = {
   },
 
   executeSettings: function(sentences) {
-
    if (this.jargon) {
-    sentences.forEach((sentence, i) => {
+      sentences.forEach((sentence, i) => {
         sentences[i] = jargonReplace(sentence);
       });
     }
 
-  if (this.grammar) {
-    sentences.forEach((sentence, i) => {
+    if (this.grammar) {
+      sentences.forEach((sentence, i) => {
         sentences[i] = grammarReplace(sentence);
       });
     }
-  }
+   }
 }
 
 
-const textInput = () => {
+/*const textInput = () => {
   return document.querySelector('#text-input');
-}
+}*/
 
 const editor = {
-  input: "",
+  input: document.querySelector('#text-input'),
+  output: document.querySelector('#text-output'),
+  counter: document.querySelector('#counter'),
 
-}
+  focusInit: function() {
+    this.input.style.color = "black";
+    this.counter.style.visibility = "visible";
 
-const initiate = () => {
-  //initializes output and counter selector
-  const textOutput = document.querySelector('#text-output');
-  const counter = document.querySelector('#counter');
-  const submitInput = document.querySelector('#submit-button');
+    this.input.addEventListener('input', () => {
+      this.count(this.input.value);
+    });
 
-  editor.input = textInput().value
-  document.querySelectorAll('.switch-input[data-toggle="setting-toggle"]').forEach(toggle => {
-  toggle.addEventListener('change', settings.changeSettings.bind(settings))});
+    this.input.addEventListener('focusout', () => {
+      if(this.input.value === "") {
+        this.input.style.color = "gray";
+        this.output.innerHTML = "";
+        this.counter.style.visibility = "hidden";
+      }
+    });
+  },
 
-  textInput().addEventListener('focusin', () => {
-    textInput().style.color = "black";
-    counter.style.visibility = "visible";
-  });
+  count: function(value) {
+    let counterMax = this.input.getAttribute('maxlength');
+    let currentCount = value.length;
 
-  textInput().addEventListener('input', () => {
-    count(textInput().value);
-  });
+    if (currentCount === counterMax){
+      this.counter.style.color = "#f15b60";
+    }
+    else {
+      this.counter.style.color = "#177bc0";
+    }
 
+    return this.counter.innerHTML = currentCount+'/'+counterMax;
 
-  submitInput.addEventListener('click', () => {
-    let errorMessage = document.querySelector('.submit-error-message');
+  },
 
-    if (textInput().value === ""){
+  submitInput: function() {
+  let errorMessage = document.querySelector('.submit-error-message');
+
+    if (this.input.value === ""){
       errorMessage.innerHTML = "Please type something first!"
       errorMessage.style.visibility = "visible";
     }
     else {
-      textOutput.innerHTML = betterSentences(textInput().value);
+      this.output.innerHTML = betterSentences(this.input.value);
       errorMessage.style.visibility = "hidden";
     }
-  });
-
-
-  //adds listenter to reset textarea when unfocused and textarea is blank and resets counter to 0
-  textInput().addEventListener('focusout', () => {
-    if(textInput().value === "") {
-      textInput().style.color = "gray";
-      textOutput.innerHTML = "";
-      counter.style.visibility = "hidden";
-    }
-  });
-
+  }
 }
 
 //Since I don't really need to share this across multiple objects, there isn't really a use to make a class here.
@@ -108,13 +107,11 @@ const initiate = () => {
 }*/
 
 const betterSentences = string => {
-
   let newParagraph = string.match(/([^\.!\?]+[\.!\?]+)|([^\.!\?]+$)/g);
   settings.executeSettings(newParagraph);
   return newParagraph.join('');
 
 }
-
 
 const jargonReplace = string => {
    return string.replace(/\butilize/ig, 'use')
@@ -135,18 +132,11 @@ const grammarReplace = string => {
 
 }
 
-const count = value => {
-  let counterMax = document.querySelector('#text-input').getAttribute('maxlength');
-  let currentCount = value.length;
-
-  if (currentCount === counterMax){
-    counter.style.color = "#f15b60";
-  }
-  else {
-    counter.style.color = "#177bc0";
-  }
-
-  return counter.innerHTML = currentCount+'/'+counterMax;
+const initiate = () => {
+  document.querySelector('#text-input').addEventListener('focusin', editor.focusInit.bind(editor))
+  document.querySelectorAll('.switch-input[data-toggle="setting-toggle"]').forEach(toggle => {
+  toggle.addEventListener('change', settings.changeSettings.bind(settings))});
+  document.querySelector('#submit-button').addEventListener('click', editor.submitInput.bind(editor))
 
 }
 

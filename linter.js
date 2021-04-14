7225e27e4878880338a435814f8b8032d801aeb3
Keegan <keegankb@gmail.com>
2021-04-14 09:15:05 -0500

    Settings box update. Cog icon. fade-in fade-out

--- linter.js
+++ linter.js
@@ -33,15 +33,15 @@ const linter = {
       }
     });
   },
-
+  //Takes two values from stored arrays in the settings obj. User inputs a remove and replace value. Then with a RegExp object we look for the word and replace it.
   userRemoveReplace: function(string) {
     for (let i = 0; i < this.removeWord.length; i++){
-      const regexRemove = new RegExp(`\\b${this.removeWord[i]}`, `ig`);
+      const regexRemove = new RegExp(`\\b${this.removeWord[i]}\\b`, `ig`);
       string = string.replace(regexRemove, this.replaceWord[i]);
     }
     return string;
   },
-
+  //executes all settings based on what the user selects.
   executeSettings: function(sentences) {
    if (this.jargon) {
       sentences.forEach((sentence, i) => {
@@ -59,18 +59,26 @@ const linter = {
         sentences[i] = this.userRemoveReplace(sentence);
    });
   },
-
+  //Is called from the reset button, resets all settings to default states.
   resetSettings: function () {
+    this.removeWord = [];
+    this.replaceWord = [];
+    this.jargon = false;
+    this.grammar = false;
+    document.querySelector('#replace-submission-container').innerHTML = "";
+    document.querySelectorAll('.switch-input[data-toggle="setting-toggle"]').forEach(toggle => {
+      toggle.checked = false;
+    });
 
   }
  },
- //editor object literal houses anything to do with the editor (input, output, buttons etc.)
+ //editor object literal houses anything to do with the functioning of the  editor (input, output, buttons etc.)
  editor: {
   input: document.querySelector('#text-input'),
   output: document.querySelector('#text-output'),
   counter: document.querySelector('#counter'),
   errorMessage: document.querySelector('.submit-error-message'),
-
+  //This method initializes a few event listeners after the user begins to type. No reason to begin listening until that has happened.
   focusInit: function() {
     this.counter.style.visibility = "visible";
     this.input.style.color = "black";
@@ -88,7 +96,7 @@ const linter = {
       }
     });
   },
-
+//This method keeps track of the amount of characters the user is typing by looking at the length of the input value.
   count: function(value) {
     let counterMax = this.input.getAttribute('maxlength');
     let currentCount = value.length;
@@ -103,33 +111,49 @@ const linter = {
     return this.counter.innerHTML = currentCount+' / '+counterMax;
 
   },
+  //This is a handler for all buttons in the editor. It takes the id of the event and executes the respective actions.
   editorButtons: function(event){
 
-    if(event.currentTarget.id === "submit-button"){
-      if (this.input.value === ""){
-        this.errorMessage.style.visibility = "visible";
-        return this.errorMessage.innerHTML = "Please type something first!"
+   if(event.currentTarget.id === "submit-button"){
+    if (this.input.value === ""){
+      this.errorMessage.style.visibility = "visible";
+      return this.errorMessage.innerHTML = "Please type something first!"
+    }
+    else {
+      this.visibility(event.currentTarget.id)
+      return this.output.innerHTML = betterSentences(this.input.value);
       }
-      else {
-        this.visibility(event.currentTarget.id)
-        this.output.innerHTML = betterSentences(this.input.value);
-      }
-     }
+    }
 
     if (event.currentTarget.id === "back-button"){
       this.visibility(event.currentTarget.id);
+      return;
     }
-
-    if (event.currentTarget.id === "reset-button"){
-      this.visibility(event.currentTarget.id);
+    if (event.currentTarget.getAttribute('data-button-type') === "reset"){
+      this.visibility(event);
       this.input.value = "";
       this.output.innerHTML = "";
       this.counter.innerHTML = "";
+      linter.settings.resetSettings();
+      return;
+    }
+
+    if(event.currentTarget.id === "replace-button"){
+      userWordRemoveInput();
+      return;
+    }
+
+    if(event.currentTarget.id === "settings-cog-button"){
+      document.querySelector('.settings-column').classList.toggle('m-fadeIn');
+
     }
 
    },
+   //this function/method controls the visibility of the elements. It's its own function (instead of a callback function within the above button handler) due to multiple buttons requiring use of manipuluating the visibility.
    visibility: function(id){
      if (id === "submit-button"){
+       document.querySelector('.settings-column').classList.toggle('m-fadeOut');
+       document.querySelector('.settings-column').classList.remove('m-fadeIn');
        document.querySelectorAll('.hide-me').forEach(element => {
           element.classList.toggle('m-fadeOut');
           element.classList.remove('m-fadeIn');
@@ -141,7 +165,7 @@ const linter = {
        return;
      }
 
-     if(id === "back-button" || id === "reset-button"){
+     if(id === "back-button" || (id.currentTarget.getAttribute('data-button-type') === "reset" && id.currentTarget.id === "reset-button")) {
        document.querySelectorAll('.hide-me').forEach(element => {
           element.classList.toggle('m-fadeIn');
           element.classList.toggle('m-fadeOut');
@@ -150,10 +174,6 @@ const linter = {
           element.classList.toggle('m-fadeOut');
           element.classList.toggle('m-fadeIn');
         });
-        /*document.querySelectorAll('.button[data-button-type="output-button"]').forEach(button => {
-          button.style.visibility = "hidden";
-          //button.style.transition = "transition: visibility 1s ease-out 3s, opacity 1s";
-      });*/
        return;
      }
    }
@@ -164,14 +184,14 @@ const linter = {
 /*String.prototype.createSentences = function () {
   return this.match(/([^\.!\?]+[\.!\?]+)|([^\.!\?]+$)/g);
 }*/
-const userWordRemoveInput = function(event) {
+const userWordRemoveInput = function() {
   const removeWordDiv = document.createElement("li");
   const wordToRemove = document.querySelector('#remove-word').value;
   const wordToReplace = document.querySelector('#replace-word').value;
-  removeWordDiv.innerHTML = wordToRemove+` &#8594; `+wordToReplace;
+  removeWordDiv.innerHTML = wordToRemove+` <i class="fas fa-rocket"></i> `+wordToReplace;
   document.body.appendChild(removeWordDiv);
-  settings.removeWord.push(document.querySelector('#remove-word').value);
-  settings.replaceWord.push(document.querySelector('#replace-word').value);
+  linter.settings.removeWord.push(document.querySelector('#remove-word').value);
+  linter.settings.replaceWord.push(document.querySelector('#replace-word').value);
   removeWords.appendChild(removeWordDiv);
   document.querySelector('#remove-word').value = "";
   document.querySelector('#replace-word').value = "";
@@ -208,7 +228,6 @@ document.querySelector('#text-input').addEventListener('focusin', linter.editor.
 document.querySelectorAll('.switch-input[data-toggle="setting-toggle"]').forEach(toggle => {
   toggle.addEventListener('change', linter.settings.changeSettings.bind(linter.settings))});
 document.querySelectorAll('.button[data-button="editor-button"]').forEach(button => {
-  console.log(button)
   button.addEventListener('click', linter.editor.editorButtons.bind(linter.editor))});
 const removeWords = document.querySelector('#replace-submission-container');
 //document.querySelector('#replace-button').addEventListener('click', userWordRemoveInput)
